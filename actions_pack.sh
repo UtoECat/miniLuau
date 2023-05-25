dd224ceab90a71e0dcaefb0d5f1ca1c8406a0578
Kuts Egor Pavlovich (UtoECat) <utopia.egor.cat.allandall@gmail.com>
2023-05-25 10:37:10 +0300

    never use git commit -a anymore...

---
+++ actions_pack.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# runs on Ubuntu Github Actions workflow
+
+# need lua
+sudo apt-get install lua5.4
+
+cd ./luau
+
+lua ../PACK.lua
+
+cp luau.hpp ../luau.hpp
+cp luau.cpp ../luau.cpp
+
+cd ..
+
+echo "Check for valid packing"
+gcc -c luau.cpp -o tmp.o -std=c++17 -Wall
