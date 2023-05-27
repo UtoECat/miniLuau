fd1f28a988d5d129013d867ac06953bdbe53a24b
Kuts Egor Pavlovich (UtoECat) <utopia.egor.cat.allandall@gmail.com>
2023-05-27 15:03:28 +0300

    silent

--- actions_pack.sh
+++ actions_pack.sh
@@ -15,9 +15,3 @@ cd ..
 
 echo "Check for valid packing"
 gcc -c luau.cpp -o tmp.o -std=c++17 -Wall
-
-echo "Make ready to commit"
-git config --global user.email "patch@auto.bot"
-git config --global user.name "Patch Bot"
-git add . 
-git commit -m "auto commit"
